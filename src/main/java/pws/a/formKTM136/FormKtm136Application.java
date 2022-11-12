399b4b1722e3296a142c1e7c741d812477b95783
Annisa Divayu Andriyani <95303337+eunannana@users.noreply.github.com>
2022-11-12 15:05:37 +0700

    first commit

---
+++ src/main/java/pws/a/formKTM136/FormKtm136Application.java
@@ -0,0 +1,13 @@
+package pws.a.formKTM136;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormKtm136Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormKtm136Application.class, args);
+	}
+
+}
---
+++ src/test/java/pws/a/formKTM136/FormKtm136ApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.a.formKTM136;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormKtm136ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
